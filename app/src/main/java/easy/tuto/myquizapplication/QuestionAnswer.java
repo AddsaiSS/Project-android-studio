982993e53ef1b32dc3cbdb34f36b27cc32c15ef9
AddsaiSS <addsai2022@gmail.com>
2023-02-26 23:13:31 +0700

    finished project

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "ບໍລິສັດໃດສສ້າງ ລະບົບ android ? ",
+            "ອັນໃດບໍ່ແມ່ນ programming language?",
+            "ເຈົ້າມັກຫລີ້ນແອັບໃດຫລາຍທີ່ສຸດ",
+            " Apple ເປັນຂອງບໍລິສັດໃດ ?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube","Facebook",
+            "Apple"
+    };
+
+}
